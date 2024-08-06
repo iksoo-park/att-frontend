7fd5b8fae7e7d3dec3e8973fd46b66933170175b
Yoonseoparkk <patpark97@gmail.com>
2024-08-06 12:54:57 +0900

    feat: [박윤서] 사용자 로그인 페이지 추가 [ATTF-5]

---
+++ src/account/pages/login/AccountLoginPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container class="container">
+        <div>
+            <v-btn class="google-login-btn" block @click="goToGoogleLogin">
+            </v-btn>
+        </div>
+    </v-container>
+</template>
+
+<script>
+import router from "@/router";
+
+export default {
+    methods: {
+        goToGoogleLogin() {
+            router.push("/google_oauth/google-access-token");
+        },
+    }
+}
+</script>
+
+
+
+<style scoped>
+.google-login-btn {
+    background-image: url("@/assets/images/fixed/googleLogin.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    align-items: center;
+    justify-content: center;
+}
+</style>
